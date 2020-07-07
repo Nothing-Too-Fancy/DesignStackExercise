66becd6947e2c753d033693177796323defdb98f
Eddie Finlaw <edrfinlaw@gmail.com>
2020-07-07 16:23:27 -0700

    Initial commit of project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignStackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStack = new Stack();
+            var integer = 6;
+            var strings = "Hello this is a string";
+            var newStack = new Stack();
+            //myStack.Push(null);
+            myStack.Push(integer);
+            myStack.Push(strings);
+            myStack.Push(newStack);
+
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+            //Console.WriteLine(myStack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace DesignStackExercise
+{
+    class Stack
+    {
+        List<object> _stack;
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+                _stack.Add(obj);
+            else
+                throw new System.InvalidOperationException("Parameter cannot be null");
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count > 0)
+            {
+                object obj = _stack[_stack.Count - 1];
+                _stack.RemoveAt(_stack.Count - 1);
+                return obj;
+            }
+            else
+                throw new System.InvalidOperationException("Stack is empty");
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
